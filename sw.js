22a1bf63be7d183678d1cecc56c91e6f68735ddc
simchorok <simchorok3@gmail.com>
2026-04-09 23:03:45 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'dal-ui-girok-v1';
+const ASSETS = [
+  '/period-tracker/',
+  '/period-tracker/index.html',
+  '/period-tracker/manifest.json',
+];
+
+// 설치 시 캐시 저장
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// 오래된 캐시 삭제
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 네트워크 우선, 실패 시 캐시 반환
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
